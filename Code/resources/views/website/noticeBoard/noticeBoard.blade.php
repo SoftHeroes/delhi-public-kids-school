3eac0257313766b45b1d8f2962eba1cacc950308
SOftHeroes <ShubhamJobanputra@gmail.com>
2019-10-16 14:38:53 -0700

    Adding Notice board Code

--- Code/resources/views/website/noticeBoard/noticeBoard.blade.php
+++ Code/resources/views/website/noticeBoard/noticeBoard.blade.php
@@ -1,23 +1,22 @@
 @section('noticeBoard')
+@php
+use Illuminate\Support\Facades\DB;
+
+$principalMessages = DB::select('SELECT * FROM principalMessages WHERE deletedAt IS NULL');
+@endphp
 <div class="container-fluid notice">
     <img class="img-fluid" src="{{ asset('website/img/board.png') }}" alt="">
     <div class="school-updates">
         <marquee behavior="" direction="up" onmouseover="stop();" onmouseout="start();" scrollamount="4">
-            <div class="singleNew">
-                <h3><a href="">Lorem ipsum dolor sit amet.</a></h3>
-                <hr>
-                <p class="text-right">posted on : 27 - 04 - 2019</p>
-            </div>
-            <div class="singleNew">
-                <h3><a href="">Lorem ipsum dolor sit amet.</a></h3>
-                <hr>
-                <p class="text-right">posted on : 27 - 04 - 2019</p>
-            </div>
-            <div class="singleNew">
-                <h3><a href="">Lorem ipsum dolor sit amet.</a></h3>
-                <hr>
-                <p class="text-right">posted on : 27 - 04 - 2019</p>
-            </div>
+            @isset($principalMessages)
+                @foreach ($principalMessages as $currentPrincipalMessages)
+                <div class="singleNew">
+                    <h3><a href="{!! route('vPrincipalMessage') !!}">{{ substr($currentPrincipalMessages->title,0,24).'...'}}</a></h3>
+                    <hr>
+                    <p class="text-right">posted on : {{$currentPrincipalMessages->createDate}}</p>
+                </div>
+                @endforeach
+            @endisset
         </marquee>
     </div>
 </div>
